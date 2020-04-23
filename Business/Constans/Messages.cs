7bf780248e25b691f46d8afe3f3bf2887e4b8093
Vacip Derici <vacipderici@outlook.com>
2020-04-23 19:00:13 +0300

    İşlem başarı ve hata sisteminin oluşturulması.

---
+++ Business/Constans/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constans
+{
+  public static class Messages
+    {
+        public static string ProductAdded = "Ürün başaralıyla eklendi.";
+
+        public static string ProductDeleted = "Ürün başaralıyla silindi.";
+
+        public static string ProductUpdated = "Ürün başarıyla güncellendi.";
+    }
+}
