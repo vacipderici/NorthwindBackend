69971513a7b75aa11ef43cc5d7e122d71b3b4418
Vacip Derici <vacipderici@outlook.com>
2020-04-24 21:50:11 +0300

    Role Yönetimi Business Configuration

--- Entities/Concrete/User.cs
+++ Entities/Concrete/User.cs
@@ -10,6 +10,7 @@ namespace Entities.Concrete
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+        public string Email { get; set; }
         public byte[] PasswordSalt { get; set; }
         public byte[] PasswordHash { get; set; }
         public bool  Status { get; set; }
