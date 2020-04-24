5cea11910ad12e04d2424c66d1bce3f10bf7d95f
Vacip Derici <vacipderici@outlook.com>
2020-04-24 22:26:32 +0300

    Role Yönetimi DTO implementasyonu

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -19,6 +19,9 @@ namespace Business.DependencyResolvers.Autofac
             builder.RegisterType<CategoryManager>().As<ICategoryService>();
             builder.RegisterType<EfCategoryDal>().As<ICategoryDal>();
 
+            builder.RegisterType<UserManager>().As<IUserService>();
+            builder.RegisterType<EfUserDal>().As<IUserDal>();
+
         }
     }
 }
---
+++ Entities/Dtos/UserForLoginDto.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Dtos
+{
+   public class UserForLoginDto :IDto //Yarın bir gün apiden ayrıldığımız zaman ortak kullanabilelim. MVC modelleri genelde Arayüzü ile alakalı olmalı. Örn Combobox değerleri. Genel modelse backende koymamız lazım.
+    {
+        public string  Email { get; set; }
+
+        public string  Password { get; set; }
+    }
+}
